711f140ac8837186d9381692edb234e1b0059b8f
cyw3 <2927096163@qq.com>
2016-01-18 15:21:55 +0800

    语法分析 BNF

--- OriStone/src/com/cyw/oristone/OriStoneException.java
+++ OriStone/src/com/cyw/oristone/OriStoneException.java
@@ -1,10 +1,12 @@
 package com.cyw.oristone;
 
+import com.cyw.oristone.ast.ASTree;
+
 public class OriStoneException extends RuntimeException {
 	public OriStoneException(String m){
 		super(m);
 	}
-//	public OriStoneException(String m, ASTree t){
-//		super(m+" "+t.location());
-//	}
+	public OriStoneException(String m, ASTree t){
+		super(m+" "+t.location());
+	}
 }
