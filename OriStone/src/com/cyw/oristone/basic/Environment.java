76b9fbdaf54b443f98299d0f5ed3cb7fe85302f0
cyw3 <2927096163@qq.com>
2016-01-18 19:55:15 +0800

    gluonj

--- OriStone/src/com/cyw/oristone/basic/Environment.java
+++ OriStone/src/com/cyw/oristone/basic/Environment.java
@@ -1,5 +1,10 @@
 package com.cyw.oristone.basic;
 
+/**
+ * 环境对象（environment），类似于变量管理器
+ * @author cyw
+ *
+ */
 public interface Environment {
     void put(String name, Object value);
     Object get(String name);
